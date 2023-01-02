9074531d72b58f9ab063cb0441a3fa25ac12751e
Robert Zondervan <robert@conduction.nl>
2023-01-02 09:28:45 +0100

    Lock, release and publish endpoints

--- Service/ZGWService.php
+++ Service/ZGWService.php
@@ -4,8 +4,16 @@
 
 namespace CommonGateway\ZGWBundle\Service;
 
+use App\Entity\ObjectEntity;
+use Doctrine\ORM\EntityManagerInterface;
+
 class ZGWService
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
 
     /*
      * Returns a welcoming string
@@ -15,5 +23,47 @@ class ZGWService
     public function zgwHandler(array $data, array $configuration): array
     {
         return ['response' => 'Hello. The ZGWBundle works'];
+    }
+
+    public function ztcPublishHandler(array $data, array $configuration): array
+    {
+        $object = $this->entityManager->getRepository('App:ObjectEntity')->find($data['response']['id']);
+        if(!$object instanceof ObjectEntity) {
+            return $data;
+        }
+        $object->hydrate(['concept' => false]);
+        $this->entityManager->persist($object);
+        $this->entityManager->flush();
+
+        $data['response'] = $object->toArray();
+        return $data;
+    }
+
+    public function drcLockHandler(array $data, array $configuration): array
+    {
+        $object = $this->entityManager->getRepository('App:ObjectEntity')->find($data['response']['id']);
+        if(!$object instanceof ObjectEntity) {
+            return $data;
+        }
+        $object->hydrate(['lock' => '{{ generated_uuid() }}']);
+        $this->entityManager->persist($object);
+        $this->entityManager->flush();
+
+        $data['response'] = $object->toArray();
+        return $data;
+    }
+
+    public function drcReleaseHandler(array $data, array $configuration): array
+    {
+        $object = $this->entityManager->getRepository('App:ObjectEntity')->find($data['response']['id']);
+        if(!$object instanceof ObjectEntity) {
+            return $data;
+        }
+        $object->hydrate(['lock' => null]);
+        $this->entityManager->persist($object);
+        $this->entityManager->flush();
+
+        $data['response'] = $object->toArray();
+        return $data;
     }
 }
